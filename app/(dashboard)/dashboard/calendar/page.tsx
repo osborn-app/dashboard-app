c0baf3e0a1d23699f4a78651896e32bea04a3e27
transgoacc <transgoacc@gmail.com>
2025-08-21 12:24:00 +0700

    triger deploy

--- app/(dashboard)/dashboard/calendar/page.tsx
+++ app/(dashboard)/dashboard/calendar/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   title: "Calendar | Transgo",
   description: "Calendar page",
 };
-
+// triger
 const page = () => {
   return (
     <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
